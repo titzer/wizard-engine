4a827d0170381a6dd44d9e380424a1768d2ed24b
Ben L. Titzer <ben.titzer@gmail.com>
2020-02-13 14:49:47 +1100

    Add unit tests for imports, improve test script

--- unit.sh
+++ unit.sh
@@ -4,6 +4,13 @@ WIZENG=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 SRC=$(find $WIZENG/src -name '*.v3')
 TEST=$(find $WIZENG/test -name '*.v3')
+LOG=/tmp/wizeng.unit.sh.log
+
+let PROGRESS_PIPE=1
+if [ "$1" = "-verbose" ]; then
+    # turn off the progress pipe in verbose mode
+    let PROGRESS_PIPE=0
+fi
 
 # Typecheck and verify wizeng first, printing out compile errors
 v3c $SRC $TEST
@@ -12,4 +19,8 @@ if [ "$?" != 0 ]; then
 fi
 
 # run unittests and pipe through progress program
-v3c -run $SRC $TEST "$@" | progress
+if [ $PROGRESS_PIPE = 1 ]; then
+    v3c -run $SRC $TEST "$@" | tee $LOG | progress
+else
+    v3c -run $SRC $TEST "$@"
+fi
