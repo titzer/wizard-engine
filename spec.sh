f4bf8d85d772b782eda212308e3fa8a184e26eb1
Ben L. Titzer <ben.titzer@gmail.com>
2020-03-19 22:50:57 +1100

    Add spec.sh and fix SpecTestRunner output

---
+++ spec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WIZENG=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
+CORE_TEST=$WIZENG/core-test/bin/
+
+if [[ "$1" =~ "-trace" ]]; then
+    # turn off the progress pipe in verbose mode
+    let PROGRESS_PIPE=0
+fi
+
+cd $CORE_TEST
+TESTS=$(ls *.wast)
+COUNT=$(echo $TESTS | awk '{print NF}')
+
+# run unittests and pipe through progress program
+echo "#${COUNT}"
+for t in $TESTS; do
+     $WIZENG/bin/wizeng-spec.x86-linux $t
+done
