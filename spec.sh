9064e35a75db9228126e63a839b7a44b14e4d798
Ben L. Titzer <ben.titzer@gmail.com>
2020-06-27 14:52:49 +1000

    Implement (ref.null) (ref.extern) in spec test parser

--- spec.sh
+++ spec.sh
@@ -3,31 +3,40 @@
 WIZENG=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 TEST_TARGET=jvm
-CORE_TEST=$WIZENG/core-test/bin/
-
-if [[ "$1" =~ "-trace" ]]; then
-    # turn off the progress pipe in verbose mode
-    let PROGRESS_PIPE=0
-    shift
-fi
 
 WIZENG_OPTS=
-while [[ "$1" =~ "-ext:" ]]; do
-    # turn off the progress pipe in verbose mode
+while [[ "$1" =~ ^\-.* ]]; do
+    if [[ "$1" =~ "\-trace" ]]; then
+	# turn off the progress pipe in verbose mode
+	let PROGRESS_PIPE=0
+    fi
     WIZENG_OPTS="$WIZENG_OPTS $1"
     shift
 done
 
-cd $CORE_TEST
-TESTS=$(ls *.bin.wast)
-COUNT=$(echo $TESTS | awk '{print NF}')
+function run {
+    BRANCH=$1
+    cd $WIZENG/wasm-spec/$BRANCH/test/core/bin/
+    TESTS=$(ls *.bin.wast)
+    COUNT=$(echo $TESTS | awk '{print NF}')
 
-# run unittests and pipe through progress program
-function run() {
-echo "##>${COUNT}"
-for t in $TESTS; do
-    $WIZENG/bin/spectest.$TEST_TARGET $WIZENG_OPTS $t
-done
+    # run unittests and pipe through progress program
+    echo "##>${COUNT}"
+    for t in $TESTS; do
+	$WIZENG/bin/spectest.$TEST_TARGET $WIZENG_OPTS $t
+    done
 }
 
-run | tee /tmp/wizeng-spec-test.out | progress
+BRANCHES="$*"
+if [ "$BRANCHES" = "" ]; then
+    BRANCHES=spec
+fi
+
+for b in $BRANCHES; do
+    if [ ! -d "$WIZENG/wasm-spec/$b" ]; then
+	echo Spec branch \"$WIZENG/wasm-spec/$b\" does not exist.
+	exit 1
+    fi
+    echo Testing $b
+    run $b | tee /tmp/wizeng-spec-$b.out | progress
+done
