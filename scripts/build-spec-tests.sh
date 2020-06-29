aaeddc7f752b169aa9d59f6d19d30f39db32e655
Ben L. Titzer <ben.titzer@gmail.com>
2020-06-29 13:43:56 +1000

    Add script to build binary spec tests

---
+++ scripts/build-spec-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BRANCHES="$@"
+if [ "$#" = 0 ]; then
+    BRANCHES=spec
+fi
+
+function build {
+    b=$1
+    echo build $b
+    DIR=${WIZENG_LOC}/wasm-spec/$b/interpreter
+    if [ ! -d "$DIR" ]; then
+	echo directory not found: $DIR
+	exit 2
+    fi
+    cd $DIR
+    make
+    if [ "$?" != 0 ]; then
+	exit $?
+    fi
+    cd ..
+    cd test/core
+    mkdir -p bin
+    TESTS=$(ls *.wast)
+    for t in $TESTS; do
+	echo   translate $t
+	../../interpreter/wasm $t -o bin/$t.bin.wast
+    done
+}
+
+for b in $BRANCHES; do
+    (build $b)
+done
