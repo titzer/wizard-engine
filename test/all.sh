4f78967981e176d346c92d3f72cfb6c7c63bea73
Ben L. Titzer <ben.titzer@gmail.com>
2020-07-24 19:44:20 +1000

    Add validation for function reference types

--- test/all.sh
+++ test/all.sh
@@ -7,5 +7,6 @@ $SCRIPT_LOC/wave.sh
 $SCRIPT_LOC/spec.sh
 $SCRIPT_LOC/spec.sh -ext:bulk-memory-operations bulk-memory-operations
 $SCRIPT_LOC/spec.sh -ext:bulk-memory-operations -ext:reference-types reference-types
+# TODO $SCRIPT_LOC/spec.sh -ext:bulk-memory-operations -ext:reference-types -ext:function-references function-references
 # TODO $SCRIPT_LOC/spec.sh -ext:tail-call tail-call
 
