7c6b2bc664e3d7b7ab07bbb48059a68941df6574
Ben L. Titzer <ben.titzer@gmail.com>
2021-04-06 19:51:24 -0500

    Hardwire bulk-memory-operations and reference-types to enabled

--- test/all.sh
+++ test/all.sh
@@ -4,9 +4,6 @@ SCRIPT_LOC=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 $SCRIPT_LOC/unit.sh
 $SCRIPT_LOC/wave.sh
-$SCRIPT_LOC/spec.sh -ext:bulk-memory-operations -ext:reference-types
-#$SCRIPT_LOC/spec.sh -ext:bulk-memory-operations bulk-memory-operations
-#$SCRIPT_LOC/spec.sh -ext:bulk-memory-operations -ext:reference-types reference-types
-# TODO $SCRIPT_LOC/spec.sh -ext:bulk-memory-operations -ext:reference-types -ext:function-references function-references
+$SCRIPT_LOC/spec.sh
+# TODO $SCRIPT_LOC/spec.sh -ext:reference-types -ext:function-references function-references
 # TODO $SCRIPT_LOC/spec.sh -ext:tail-call tail-call
-
