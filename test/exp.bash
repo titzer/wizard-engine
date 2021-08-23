fb034dab8c01150482b7d5e695b62d6502ab13d4
Ben L. Titzer <ben.titzer@gmail.com>
2021-08-22 23:13:14 -0500

    test/exp.bash

--- test/exp.bash
+++ test/exp.bash
@@ -2,15 +2,17 @@
 
 SCRIPT_LOC=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
+#TODO: out of memory TEST_TARGET=int $SCRIPT_LOC/spec.sh
 TEST_TARGET=x86-linux $SCRIPT_LOC/spec.sh
 TEST_TARGET=x86-64-linux $SCRIPT_LOC/spec.sh
+TEST_TARGET=jvm $SCRIPT_LOC/spec.sh
 
 TEST_TARGET=int $SCRIPT_LOC/wizeng.sh
 TEST_TARGET=x86-linux $SCRIPT_LOC/wizeng.sh
 TEST_TARGET=x86-64-linux $SCRIPT_LOC/wizeng.sh
+TEST_TARGET=jvm $SCRIPT_LOC/wizeng.sh
 
 TEST_TARGET=int $SCRIPT_LOC/unit.sh
 TEST_TARGET=x86-linux $SCRIPT_LOC/unit.sh
 TEST_TARGET=x86-64-linux $SCRIPT_LOC/unit.sh
-
-#TEST_TARGET=x86-linux $SCRIPT_LOC/spec.sh
+#TODO: v3 heap too large TEST_TARGET=jvm $SCRIPT_LOC/unit.sh
