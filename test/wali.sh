d4f736992997acc1d79f2a948450513fc4ab2295
Muhammad Sulthan Mazaya <msulthanmazaya@gmail.com>
2025-05-02 12:25:34 -0400

    Add mem offset flag to wali.sh

--- test/wali.sh
+++ test/wali.sh
@@ -43,14 +43,14 @@ function run_test() {
     if [ -z $TEST_FILE ]; then
         for file in "$TEST_DIR"/*.v3; do
           if [ -f "$file" ]; then
-              $CUR_V3C -heap-size=64k -symbols -target=wasm $file
+              $CUR_V3C -wasm.mem-offset=2048 -heap-size=64k -symbols -target=wasm $file
               if [ $? -ne 0 ]; then
               exit 1
               fi
           fi
         done
     else
-      $CUR_V3C -heap-size=64k -symbols -target=wasm $TEST_FILE
+      $CUR_V3C -wasm.mem-offset=2048 -heap-size=64k -symbols -target=wasm $TEST_FILE
       if [ $? -ne 0 ]; then
       exit 1
       fi
