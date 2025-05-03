56fbfdc682cabfe90229c8be2d2d5220f84f2d42
Ben L. Titzer <ben.titzer@gmail.com>
2025-05-03 14:29:32 -0400

    [ci] Run spec tests separately (#380)

--- ci/build-test-spec.sh
+++ ci/build-test-spec.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage: build-test.sh <target>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: build-test-spec.sh <target> <proposal>"
     exit 1
 fi
 
 set -e
 
+# for CI, use line-by-line test output
+export PROGRESS_ARGS=l
+
 # first argument must be the target
 export TEST_TARGET=$1
 shift
 
-# for CI, use line-by-line test output
-export PROGRESS_ARGS=l
+export PROPOSAL=$1
+shift
 
 #############################################################################
 # Set up latest version of virgil
@@ -34,25 +37,28 @@ make
 
 # remaining arguments are appended to V3C_OPTS
 export V3C_OPTS="$V3C_OPTS $@"
-popd
+popd 
 #############################################################################
 
 # Set up wizard
 make -j $TEST_TARGET
 
 # Install Opam for specification tests
-sudo apt install -y opam
-opam init
+if [ "$(which opam)" = "" ]; then
+    sudo apt install -y opam
+    opam init
 
-# Install spec test dependencies
-opam install dune
-opam install menhir -y
-opam install ocamlbuild
+    # Install spec test dependencies
+    opam install dune
+    opam install ocamlbuild
+    opam install menhir -y
+fi
+
 # Make dune available in PATH
 eval $(opam config env)
 
 # Set up specification tests
-./test/wasm-spec/update.sh
+./test/wasm-spec/update.sh "$PROPOSAL"
 
 # Run all tests
-./test/all.sh
+exec ./test/proposal.sh "$PROPOSAL"
