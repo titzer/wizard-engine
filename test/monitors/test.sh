c23f0e2c6d239d79c28152fd66d7fade3e940106
Matthew Schneider <panat.matthew@gmail.com>
2025-08-26 10:54:32 -0400

    Add interpreter requiring abstract interpreter to tests

--- test/monitors/test.sh
+++ test/monitors/test.sh
@@ -28,7 +28,8 @@ else
         "branches" "branches{c}" 
         "hotness" "hotness{c}"
         "globals" 
-        "profile_bytecode{switch_size=16}")
+        "profile_bytecode{switch_size=7}"
+	"profile_bytecode{switch_size=7,abstract_interpreter=true}")
 fi
 
 TESTS=$(ls *.wasm)
